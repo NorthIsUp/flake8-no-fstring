38c3af5ed34a359e6f6154b1c6b0ae963aac0d3b
Adam Hitchcock <adam@northisup.com>
2020-09-12 22:46:02 -0700

    Fix incorrect detection of a format inside a fstring

--- src/flake8_no_unnecessary_fstrings.py
+++ src/flake8_no_unnecessary_fstrings.py
@@ -1,5 +1,6 @@
 import ast
 import sys
+from collections import deque
 
 if sys.version_info >= (3, 8):
     from importlib.metadata import version
@@ -13,22 +14,33 @@ class NoUnnecessaryFstringChecker(object):
     """
 
     name = "flake8-no-unnecessary-fstrings"
-    version = version("flake8-no-unnecessary-fstrings")
+    version = version(name)
+
+    message_NUF001 = "NUF001 f-string without interpolation."
 
     def __init__(self, tree, *args, **kwargs):
         self.tree = tree
 
-    message_NUF001 = "NUF001 No f-strings without interpolation."
+    def check_joinedstring_has_formatted_value(self, node: ast.JoinedStr) -> bool:
+        return any(isinstance(value, ast.FormattedValue) for value in ast.walk(node))
+
+    def nuf001_msg(self, node):
+        return (
+            node.lineno,
+            node.col_offset,
+            self.message_NUF001,
+            type(self),
+        )
 
     def run(self):
-        for node in ast.walk(self.tree):
+        todo = deque([self.tree])
+        while todo:
+            node = todo.popleft()
+
             if isinstance(node, ast.JoinedStr):
-                if not any(
-                    isinstance(value, ast.FormattedValue) for value in node.values
-                ):
-                    yield (
-                        node.lineno,
-                        node.col_offset,
-                        self.message_NUF001,
-                        type(self),
-                    )
+                is_good = self.check_joinedstring_has_formatted_value(node)
+                if not is_good:
+                    yield self.nuf001_msg(node)
+            else:
+                # only visit children of non-JoinedStr nodes
+                todo.extend(ast.iter_child_nodes(node))
--- tests/test_flake8_no_unnecessary_fstrings.py
+++ tests/test_flake8_no_unnecessary_fstrings.py
@@ -1,5 +1,6 @@
 import sys
 from inspect import cleandoc
+from typing import Sequence, Tuple
 
 import pytest
 
@@ -27,12 +28,13 @@ FAIL = object()
 
 
 class Scenario:
-    msg = "NUF001 No f-strings without interpolation."
+    msg = "NUF001 f-string without interpolation."
 
-    def __init__(self, name, python, should=None, expects=None):
+    def __init__(self, name, python, should=None, expects=None, xfail=False) -> None:
         self.name = name
         self.python = cleandoc(python)
         self.should = should
+        self.xfail = xfail
 
         if should is PASS:
             expects = expects or ()
@@ -42,17 +44,21 @@ class Scenario:
 
         self.expects = expects or ()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.name
 
     @property
-    def test_name(self):
-        prefix = "" if self.should is PASS else "no "
-        return f"{prefix}catch {self.name}"
+    def test_name(self) -> str:
+        prefix = "allow" if self.should is PASS else "disallow"
+        return f"{prefix} {self.name}"
 
     @classmethod
-    def expects_msg(cls, line=1, col=1):
+    def expects_msg(cls, line: int = 1, col: int = 1) -> str:
         return f"./example.py:{line}:{col}: {cls.msg}"
+
+    @classmethod
+    def expects_msgs(cls, *lines_cols: Sequence[Tuple[int, int]]):
+        return [cls.expects_msg(l, c) for (l, c) in lines_cols]
 
 
 # NUF001
@@ -95,11 +101,45 @@ scenarios = [
         expects=[Scenario.expects_msg(2, 5)],
     ),
     Scenario("regular format", '"{val}".format(1)', should=PASS),
+    Scenario(
+        "inner format string '.3f'",
+        """
+        import time
+        import_start = time.time()
+        import_end = time.time()
+        cmd_name = "hi how are you?"
+        f"{0.123456789:.3f}"
+        f'Took {import_end - import_start:.3f} to import {cmd_name}'
+        """,
+        should=PASS,
+    ),
+    Scenario(
+        "double bad strings",
+        """
+        f''
+        f''
+        """,
+        should=FAIL,
+        expects=[Scenario.expects_msg(1, 1), Scenario.expects_msg(2, 1)],
+    ),
+    Scenario(
+        "?",
+        """
+        f"{f''}"
+        """,
+        should=FAIL,
+        xfail="nested fstrings is not yet detected",
+    ),
 ]
 
 
 @pytest.mark.parametrize("scenario", scenarios, ids=lambda s: s.test_name)
 def test_NUF001(scenario, flake8dir):
+    if scenario.xfail:
+        pytest.xfail(
+            scenario.xfail if isinstance(scenario.xfail, str) else "known failure"
+        )
+
     flake8dir.make_example_py(cleandoc(scenario.python))
     result = flake8dir.run_flake8()
     assert result.out_lines == list(scenario.expects)
